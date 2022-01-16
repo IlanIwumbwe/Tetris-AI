1c063f36f3bc4bfd38181ea58091acd854f4611a
Ilan Iwumbwe <56346800+IlanIwumbwe@users.noreply.github.com>
2022-01-16 13:53:40 +0000

    Update tetris_ai.py

--- ttris/tetris_ai.py
+++ ttris/tetris_ai.py
@@ -582,9 +582,8 @@ class Tetris:
 
         self.current_piece = self.next_piece
 
-        if [WHITE] * 10 == self.grid[1] and [WHITE] * 10 == self.grid[0]:
-            self.next_piece = self.generate.get_piece()
-            self.change_piece = False
+        self.next_piece = self.generate.get_piece()
+        self.change_piece = False
 
     def game_logic(self):
         self.grid = self.board.create_grid()

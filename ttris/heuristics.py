50cbbc3d6084028823e458d8b00ed98dd5d79a17
Ilan Iwumbwe <56346800+IlanIwumbwe@users.noreply.github.com>
2022-02-15 23:14:43 +0000

    Update heuristics.py

--- ttris/heuristics.py
+++ ttris/heuristics.py
@@ -17,9 +17,12 @@ class Heuristics:
                 return self.height - row
         return 0
 
-    # aggr height
-    def aggr_height(self):
-        return sum([self.column_height(col) for col in range(self.width)])
+    # max height
+    def max_height(self):
+        return max([self.column_height(col) for col in range(self.width)])
+
+    def min_height(self):
+        return min([self.column_height(col) for col in range(self.width)])
 
     # number of holes in a column
     def column_holes(self, column):
@@ -61,32 +64,33 @@ class Heuristics:
 
         return pits
 
-    # number of wells
-    def wells(self):
-        col_heights = [self.column_height(col) for col in range(self.width)]
-        wells = []
-        for j in range(len(col_heights)):
-            if j == 0:
-                depth = col_heights[1] - col_heights[0]
-                depth = depth if depth > 0 else 0
-                wells.append(depth)
-            elif j == len(col_heights) - 1:
-                depth = col_heights[-2] - col_heights[-1]
-                depth = depth if depth > 0 else 0
-                wells.append(depth)
-            else:
-                depth1 = col_heights[j-1] - col_heights[j]
-                depth2 = col_heights[j+1] - col_heights[j]
-                depth1 = depth1 if depth1 > 0 else 0
-                depth2 = depth2 if depth2 > 0 else 0
-                depth = depth1 if depth1 >= depth2 else depth2
-                wells.append(depth)
+    # row transitions
+    def row_transitions(self):
+        transitions = 0
+        for row in self.field:
+            if 1 in row:
+                for col in range(self.width-1):
+                    if row[col] == 0 and row[col+1] == 1:
+                        transitions += 1
+                    elif row[col] == 1 and row[col+1] == 0:
+                        transitions += 1
+        return transitions
 
-        return wells
+    # col transitions
+    def col_transitions(self):
+        transitions = 0
 
-    # deepest well
-    def deepest_well(self):
-        return max(self.wells())
+        for col in range(self.width):
+            for row in range(self.height-1):
+                if self.field[row][col] == 1 and self.field[row+1][col] == 0:
+                    transitions += 1
+                elif self.field[row][col] == 0 and self.field[row+1][col] == 1:
+                    transitions += 1
+
+        return transitions
+
+    def total_height(self):
+        return sum([self.column_height(i) for i in range(self.width)])
 
     # lines cleared
     def lines_cleared(self):
@@ -99,28 +103,24 @@ class Heuristics:
         return lines
 
     def print_stats(self):
-        print(f'a. Agg: {self.aggr_height()} \nb. Total Holes: {self.total_holes()} \nc. Bumpiness: {self.bumpiness()} '
+        print(f'a. Max H: {self.max_height()} \nb. Total Holes: {self.total_holes()} \nc. Bumpiness: {self.bumpiness()} '
               f'\nd. Heights stdDEV:{self.std_heights()} \nPits: {self.pits()}')
 
     def print_num_grid(self):
         print(' NEW FRAME ....................................')
         for i in self.field:
-            print(f'{i} \n')
+            print(i)
 
     def get_heuristics(self):
         # grid_data = self.empty()
-        return np.array([self.aggr_height(), self.total_holes(), self.bumpiness(), self.lines_cleared()])
+        return [self.total_height(), self.total_holes(), self.bumpiness(), self.lines_cleared(), self.row_transitions(), self.std_heights(), self.pits(), self.col_transitions()]
 
-    def empty(self):
-        em = [0 for _ in range(200)]
+    # number of blocks
+    def total_blocks(self):
+        total = 0
+        for i in range(self.height):
+            for col in self.field[i]:
+                if col == 1:
+                    total += 1
 
-        for ind_y in range(2, self.height-2):
-            for ind_x in range(self.width):
-                if self.field[ind_y][ind_x] == 1:
-                    ind = 10*(ind_y-2) + ind_x
-                    em[ind] = 1
-
-        return em
-
-    def get_reward(self):
-        return sum([self.aggr_height()*-500, self.total_holes()*-250, self.bumpiness()*-300, self.std_heights()*-500, self.pits()*30, self.deepest_well()*30, self.lines_cleared()*6000])
+        return total

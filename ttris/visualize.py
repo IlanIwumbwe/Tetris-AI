6d6d541a5ced594a37901a6abbf542d2f61ff8a2
Ilan Iwumbwe <56346800+IlanIwumbwe@users.noreply.github.com>
2022-04-08 22:46:24 +0100

    Update visualize.py

--- ttris/visualize.py
+++ ttris/visualize.py
@@ -13,22 +13,24 @@ class Visualize:
         style.use('ggplot')
 
         for ind, e in enumerate(self.epochs):
+            # 1000 is number of genomes in population, change if needed
             max_fitness = max(self.fl[ind])
-            plt.scatter([e for _ in range(1000)], self.fl[ind], color='purple', label='All fitnesses per epoch')
-            plt.plot([e], [max_fitness], color='red',label='Max fitness per epoch')
+            plt.scatter([e for _ in range(1000)], self.fl[ind], color='lightseagreen', label='All fitnesses per epoch')
+            plt.plot([e], [max_fitness], color='lightseagreen',label='Max fitness per epoch',marker='.')
 
-        plt.plot(self.epochs, self.afl, marker=".", label='Average fitness per epoch')
+        plt.plot(self.epochs, self.afl, marker=".", label='Average fitness per epoch',color='gold')
         plt.xlabel('Epochs')
         plt.ylabel('Fitness')
         plt.legend()
         plt.show()
 
         for ind, e in enumerate(self.epochs):
+            # 1000 is number of genomes in population, change if needed
             max_score = max(self.sl[ind])
-            plt.scatter([e for _ in range(1000)], self.sl[ind], color='purple', label='All scores per epoch')
-            plt.plot([e], [max_score], color='red',label='Max score per epoch')
+            plt.scatter([e for _ in range(1000)], self.sl[ind], color='lightskyblue', label='All scores per epoch')
+            plt.plot([e], [max_score], color='lightskyblue',label='Max score per epoch',marker='.') 
 
-        plt.plot(self.epochs, self.asl, marker=".", label='Average score per epoch')
+        plt.plot(self.epochs, self.asl, marker=".", label='Average score per epoch',color='gold')
         plt.xlabel('Epochs')
         plt.ylabel('Score')
         plt.legend()
